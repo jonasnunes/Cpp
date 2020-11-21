14d030b5311a62619a9fd26172cd702d445cf7b9
Jonas Nunes <jonasnunes1030@users.noreply.github.com>
2020-11-21 14:16:57 -0300

    Update apresentando.cpp

--- apresentando.cpp
+++ apresentando.cpp
@@ -6,15 +6,19 @@ int main()
 {
     setlocale(LC_CTYPE, "Portuguese");
 
-    string nome;
-    int idade;
+    string nome, mes;
+    int dia, ano;
 
     cout << "Olá, Seja muito bem vindo!" << endl;
     cout << "Qual é o seu nome?" << endl;
     cin >> nome;
-    cout << "Quantos anos você tem?" << endl;
-    cin >> idade;
-    cout << "Muito obrigado " << nome << "!";
+    cout << "Em que dia você nasceu?" << endl;
+    cin >> dia;
+    cout << "De qual mês? " << endl;
+    cin >> mes;
+    cout << "Em qual ano? " << endl;
+    cin >> ano;
+    cout << "Muito obrigado! " << nome << ", você nasceu no dia " << dia << " do mês de " << mes << " no ano de " << ano << endl;
 
     return 0;
 }
